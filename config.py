f61a8dbc176ce43e07ee1a23ba0add6b9c611879
monologg <adieujw@gmail.com>
2018-11-07 00:27:39 +0900

    config change

--- config.py
+++ config.py
@@ -12,16 +12,16 @@ optimizer = 'adam'
 use_pretrained = True  # If you're using pretrained, emb_dim will be 200 for PubMed-and-PMC-w2v.bin (http://evexdb.org/pmresources/vec-space-models/)
 dev_size = 0.1
 hidden_unit_size = 256
-use_batch_norm = False
+use_batch_norm = True
 dropout_rate = 0.5
 
 # l2 regularizer setting
-use_l2_reg = False
-reg_coef_conv = 0.0001
-reg_coef_dense = 0.0001
+use_l2_reg = True
+reg_coef_conv = 1e-7
+reg_coef_dense = 1e-7
 
 # 2. CNN specific
-kernel_lst = [3, 5, 7, 9]  # [3, 5, 7]
+kernel_lst = [6, 7, 8, 9]  # [3, 5, 7]
 nb_filters = 200
 
 # 3. RNN specific
@@ -45,7 +45,7 @@ callback_list = [
     # 2. Model Checkpoint
     ModelCheckpoint(filepath=os.path.join(result_dir, 'weights.h5'), monitor='val_loss', save_best_only=True),
     # 3. Reducing Learning rate automatically
-    ReduceLROnPlateau(monitor='val_loss', patience=3, factor=0.5),  # Reduce the lr_rate into 10%
+    ReduceLROnPlateau(monitor='val_loss', patience=1, factor=0.9),  # Reduce the lr_rate into 10%
     # 4. Tensorboard callback
     # TensorBoard(log_dir=tf_board_dir, histogram_freq=0, write_graph=True, write_grads=True, write_images=True)
 ]
